009715871be0ef215027b56637365cc5786fcc59
teiatan <92535482+teiatan@users.noreply.github.com>
2022-10-04 20:38:36 +0300

    task1 markup is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// creating collection of pictures 
+const previewImagesContainer = document.querySelector(`.gallery`);
+const galleryItemsEl = [];
+
+for (let galleryItem of galleryItems) {
+    const galleryItemEl = document.createElement(`div`);
+    galleryItemEl.classList.add(`gallery__item`);
+
+    const galleryLinkEl = document.createElement(`a`);
+    galleryLinkEl.classList.add(`gallery__link`);
+    galleryLinkEl.href = galleryItem.original;
+
+    const galleryImgEl = document.createElement(`img`);
+    galleryImgEl.classList.add(`gallery__image`);
+    galleryImgEl.src = galleryItem.preview;
+    galleryLinkEl.dataSource = galleryItem.original;
+    galleryImgEl.alt = galleryItem.description;
+
+    galleryItemEl.appendChild(galleryLinkEl);
+    galleryLinkEl.appendChild(galleryImgEl);
+
+    galleryItemsEl.push(galleryItemEl);
+
+}
+
+previewImagesContainer.append(...galleryItemsEl);
+
+// delegation
+
+
+console.log(galleryItemsEl);
+
