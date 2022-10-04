83e27b498893a8b488fcf1e0ab5bf7a2b5cee865
teiatan <92535482+teiatan@users.noreply.github.com>
2022-10-04 20:44:45 +0300

    task2 markup is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// creating collection of pictures 
+const previewImagesContainer = document.querySelector(`.gallery`);
+const galleryItemsEl = [];
+
+for (let galleryItem of galleryItems) {
+    const galleryItemEl = document.createElement(`li`);
+    
+
+    const galleryLinkEl = document.createElement(`a`);
+    galleryLinkEl.classList.add(`gallery__item`);
+    galleryLinkEl.href = galleryItem.original;
+
+    const galleryImgEl = document.createElement(`img`);
+    galleryImgEl.classList.add(`gallery__image`);
+    galleryImgEl.src = galleryItem.preview;
+    galleryImgEl.alt = galleryItem.description;
+
+    galleryItemEl.appendChild(galleryLinkEl);
+    galleryLinkEl.appendChild(galleryImgEl);
+
+    galleryItemsEl.push(galleryItemEl);
+
+}
+
+previewImagesContainer.append(...galleryItemsEl);
+
+// delegation
+
+
+console.log(galleryItemsEl);
+
